383e011c974ebe5a8cf539664bb51b6d19571a0a
Anthony <xd-4-15@hotmail.com>
2021-01-10 11:34:53 -0500

    Modificamos un poco el codigo del ahorcado

--- practicas/ahorcado.py
+++ practicas/ahorcado.py
@@ -49,24 +49,33 @@ IMÁGENES_AHORCADO = ['''
        |
 =========''']
 
-guiones = [] 
+guiones = ""
+import threading
 
-def mostrar_ahorcado(intentos, palabra_user, letra_user):
-    for i in range(len(palabra_user)):
-        if letra_user == palabra_user[i]:
-            guiones[i] = letra_user
-    print(guiones)
-    print(IMÁGENES_AHORCADO[intentos])
+def mostrar_ahorcado():
+    for i in range(7):
+        print(IMÁGENES_AHORCADO[i])
 
 def verificar_letra(intento, palabra_user):
+    global guiones
     letra_user = input("Ingresa una letra: ")
-    mostrar_ahorcado(intento, palabra_user, letra_user)
+    
+    if(letra_user in guiones):
+        return  "La letra ingresa ya ha sido adivinada" + "\n" + guiones 
+    else:
+        for i in range(len(palabra_user)):
+            if(letra_user == palabra_user[i]):
+                guiones = guiones.split(" ")
+                guiones[i] = letra_user
+                guiones = ' '.join(guiones)
+
+        return guiones
 
 
 palabra_user = input("Ingrese una palabra: ")
 for i in range(len(palabra_user)):
     if i <= len(palabra_user):
-        guiones.append("_")
+        guiones += "_ "
 
 for i in range(len(palabra_user)):
-    verificar_letra(i, palabra_user)
+    print(verificar_letra(i, palabra_user))
